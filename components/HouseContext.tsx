04bc42ef95e915f84036595626fcbc02b179a219
g.musciagli <gionmuscia@gmail.com>
2023-07-30 00:54:38 +0200

    HouseContext.tsx implementation

--- components/HouseContext.tsx
+++ components/HouseContext.tsx
@@ -3,9 +3,14 @@
 import React, {useState, useEffect, createContext} from "react";
 import {ReactElement} from "react";
 
+import { housesData } from "../data";
 
-const HouseContext = (): ReactElement => {
-    return <div> HouseContext </div>
+// create context
+// @ts-ignore
+export const HouseContext = createContext();
+
+const HouseContextProvider = ({children} : any): ReactElement => {
+    return <HouseContext.Provider value={console.log("this is the context")}> {children} </HouseContext.Provider>
 };
 
-export default HouseContext;
+export default HouseContextProvider;
